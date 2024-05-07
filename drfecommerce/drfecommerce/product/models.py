d0d91d1e576ed1a1455ddac8da8f694c2e307f58
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-07 13:51:06 -0500

    updated product model

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     is_digital = models.BooleanField(default=False)
-    category = TreeForeignKey('Category', null=True, blank=True)
+    category = TreeForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
 
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     def __str__(self):
