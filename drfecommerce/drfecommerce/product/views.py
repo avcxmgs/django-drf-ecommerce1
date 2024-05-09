914eccf6c5b16b57695d7ddbc088142d1cd5a660
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-09 10:51:06 -0500

    modified models, serializers, and views

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
+from rest_framework.decorators import action
+
 from .models import Category, Brand, Product
 from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
 
@@ -40,9 +41,28 @@ class ProductViewSet(viewsets.ViewSet):
     """
 
     queryset = Product.objects.all()
+    lookup_field = 'slug'
+
+    def retrieve(self, request, slug=None):
+        serializer = ProductSerializer(self.queryset.filter(slug=slug), many=True)
+        return Response(serializer.data)
 
     @extend_schema(responses=ProductSerializer)
 
     def list(self, request):
         serializer = ProductSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+    
+    @action(
+        methods=['get'],
+        detail=False,
+        url_path=r"category/(?P<category>\w+)/all",
+    )
+    def list_product_by_category(self, request, category=None):
+        """
+        An endpoint to return products by category
+        """
+        serializer = ProductSerializer(
+            self.queryset.filter(category__name=category), many=True
+        )
         return Response(serializer.data)
