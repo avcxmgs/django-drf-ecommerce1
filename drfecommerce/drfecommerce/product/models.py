53bd032928d5115de803f4a2228f9a49c021b77a
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-13 08:33:15 -0500

    nc

--- drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/product/models.py
@@ -1,5 +1,8 @@
+from typing import Collection
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+from .fields import OrderField
+from django.core.exceptions import ValidationError
 
 
 class ActiveQueryset(models.QuerySet):
@@ -9,6 +12,7 @@ class ActiveQueryset(models.QuerySet):
 
 class Category(MPTTModel):
     name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=255)
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
     is_active = models.BooleanField(default=False)
 
@@ -56,3 +60,18 @@ class ProductLine(models.Model):
         Product, on_delete=models.CASCADE, related_name='product_line'
     )
     is_active = models.BooleanField(default=False)
+    order = OrderField(unique_for_field='product', blank=True)
+    objects = ActiveQueryset.as_manager()
+
+    def clean(self):
+        qs = ProductLine.objects.filter(product=self.product)
+        for obj in qs:
+            if self.id != obj.id and self.order == obj.order:
+                raise ValidationError('Duplicate value.')
+            
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(ProductLine, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.sku)
