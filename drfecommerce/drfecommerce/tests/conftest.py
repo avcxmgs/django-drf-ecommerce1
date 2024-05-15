0cf59fa8cc9855771822eca99c074632ca355299
Margareth Santos <maggie_snts@yahoo.ca>
2024-05-15 08:40:34 -0500

    dev-1.0.4 completed

--- drfecommerce/drfecommerce/tests/conftest.py
+++ drfecommerce/drfecommerce/tests/conftest.py
@@ -2,13 +2,17 @@ import pytest
 from pytest_factoryboy import register
 from rest_framework.test import APIClient
 
-from .factories import CategoryFactory, BrandFactory, ProductFactory, ProductLineFactory, ProductImageFactory
+from .factories import (CategoryFactory, BrandFactory, ProductFactory, ProductLineFactory,
+    ProductImageFactory, AttributeFactory, AttributeValueFactory, ProductTypeFactory)
 
 register(CategoryFactory)
 register(BrandFactory)
 register(ProductFactory)
 register(ProductLineFactory)
 register(ProductImageFactory)
+register(ProductTypeFactory)
+register(AttributeFactory)
+register(AttributeValueFactory)
 
 @pytest.fixture
 def api_client():
--- drfecommerce/drfecommerce/tests/factories.py
+++ drfecommerce/drfecommerce/tests/factories.py
@@ -1,6 +1,16 @@
 import factory
 
-from drfecommerce.product.models import Category, Brand, Product, ProductLine, ProductImage
+from drfecommerce.product.models import (
+    Category,
+    Brand,
+    Product,
+    ProductLine,
+    ProductImage,
+    ProductType,
+    Attribute,
+    AttributeValue
+)
+
 
 class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -16,16 +26,46 @@ class BrandFactory(factory.django.DjangoModelFactory):
     name = factory.Sequence(lambda n: 'Brand_%d' % n)
 
 
+class AttributeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Attribute
+
+    name = 'attribute_name_test'
+    description = 'attr_description_test'
+
+
+class ProductTypeFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = ProductType
+
+    name = "test_type"
+
+    @factory.post_generation
+    def attribute(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+        self.attribute.add(*extracted)
+
+
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Product
 
-    name = 'test_product'
-    description = 'test_description'
+    name = "test_product"
+    description = "test_description"
     is_digital = True
     brand = factory.SubFactory(BrandFactory)
     category = factory.SubFactory(CategoryFactory)
     is_active = True
+    product_type = factory.SubFactory(ProductTypeFactory)
+
+
+class AttributeValueFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = AttributeValue
+
+    attribute_value = 'attr_test'
+    attribute = factory.SubFactory(AttributeFactory)
 
 
 class ProductLineFactory(factory.django.DjangoModelFactory):
@@ -33,10 +73,16 @@ class ProductLineFactory(factory.django.DjangoModelFactory):
         model = ProductLine
 
     price = 10.00
-    sku = '12345'
+    sku = "12345"
     stock_qty = 1
     product = factory.SubFactory(ProductFactory)
     is_active = True
+
+    @factory.post_generation
+    def attribute_value(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+        self.attribute_value.add(*extracted)
 
 
 class ProductImageFactory(factory.django.DjangoModelFactory):
